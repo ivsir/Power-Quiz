0e624bf02b204129e073708fdc1f970c7437b152
Risvi-Tareq <ivsirqerat@gmail.com>
2022-10-21 10:48:27 -0700

    fixed typos so that each question is displayed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,91 +1,3 @@
-// var startButton = document.getElementById("start-btn");
-// var questionContainer = document.getElementById("question-container");
-// var questionElement = document.getElementById("question");
-// var answerButtonsElement = document.getElementById("answer-buttons");
-
-// var questionIndex, currentQuestion;
-
-// var acceptedAnswer = true;
-// var timer = 60;
-
-// var availableQuestions = [];
-// var questions = [
-//   {
-//     question: "What is the first law of power?",
-//     answers: [
-//       { text: "Never outshine the master.", correct: true },
-//       { text: "Concentrate your forces.", correct: false },
-//       { text: "Play to people's fantasies", correct: false },
-//       { text: "Avoid the unhappy and unlucky", correct: false },
-//     ],
-//   },
-//   {
-//     question: "What is the 25th law of power?",
-//     answers: [
-//       {
-//         text:
-//           "Never put too much trust in friends, learn how to use enemies.",
-//
-//       },
-//       { text: "Concentrate your forces.", correct: false },
-//       { text: "Re-create yourself", correct: true },
-//       { text: "Avoid the unhappy and unlucky", correct: false },
-//     ],
-//   },
-//   {
-//     question:
-//       "Which law is this referring to? : He lives well who conceals himself well.",
-//     answers: [
-//       {
-//         text:
-//           "Never put too much trust in friends, learn how to use enemies.",
-//         correct: false,
-//       },
-//       { text: "Avoid the unhappy and unlucky", correct: false },
-//       { text: "Re-create yourself", correct: false },
-//       { text: "Think as you like but behave like others", correct: true },
-//     ],
-//   },
-// ];
-// var max_questions = 4;
-
-// startButton.addEventListener("click", startGame);
-
-// // start the gameÇ
-
-// function startGame() {
-//   startButton.classList.add("hide");
-//   questionContainer.classList.remove("hide");
-
-//   // pick a random question
-//   questionIndex = Math.floor(Math.random() * questions.length);
-// //   var currentQuestion = questions[questionIndex];
-// //   question.innerText = currentQuestion.question;
-//   setNextQuestion();
-
-// //   question.answers.forEach(answer => {
-// //     var button = document.createElement("button");
-// //     button.innerText = answer.text;
-// //     button.classList.add("btn");
-// //     if(answer.correct) {
-// //         button.dataset.correct = answer.correct
-// //     }
-// //     button.addEventListener("click", selectAnswer)
-// //     answerButtonsElement.appendChild(button);
-// //   });
-// }
-
-// // set next question
-// function setNextQuestion() {
-//     showQuestion(questions[questionIndex]);
-// }
-
-// function showQuestion(question) {
-//     question.innerText = currentQuestion.question;
-// }
-// // select the answer
-// function selectAnswer(e) {}
-
 var question = document.querySelector("#question");
 var choices = Array.from(document.querySelectorAll(".choice-text"));
 var progressText = document.querySelector("#progressText");
@@ -157,39 +69,42 @@ getNewQuestion = () => {
 
     questionCounter++;
     progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`;
-    // progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%`;
     
+    // assigns question to the question element
     var questionIndex = Math.floor(Math.random() * availableQuestions.length);
     var currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
+    // assigns each choice in a box corresponding to its number
     choices.forEach(choice => {
-        var number = choice.dataset ["number"];
+        var number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
     })
 
+    // removes the question from the available questions array
     availableQuestions.splice(questionIndex,1);
 
     acceptedAnswer = true;
 }
 
 choices.forEach(choice => {
-  choice.addEventListener("click", e => {
-    if(!acceptedAnswers) return;
-    acceptedAnswers = false;
-    var selectedChoice=e.target;
-    var selectedAnswer=selectedChoice.dataset("number");
+  choice.addEventListener("click", function(event){
+    if(!acceptedAnswer) return;
 
-    var classToApply = selectedAnswer == currentQuestion.answer ? "correct" : 'incorrect';
+    acceptedAnswer = false;
+    var selectedChoice = event.target
+    var selectedAnswer = selectedChoice.dataset["number"];
+
+    var classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
     if (classToApply === "correct") {
-      incrementScore(score_points)
+      incrementScore(score_points);
     }
 
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
-      selectedChoice.parentElement.classLIst.remove(classToApply);
+      selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
     }, 1000)
   })
